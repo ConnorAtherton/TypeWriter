c51c0e1e2f666802052c4278fef86bceed464485
Connor Atherton <c.liam.atherton@gmail.com>
2014-04-07 21:41:15 +0100

    Add more info to README

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
 TypeWriter
 ==========
+
+TypeWriter provides an easy way to print out text on the web. It comes with a
+few different configuration options that are all documented below.
+
+See some examples [here](http://connoratherton/typewriter).
+
+# Letter by letter, fixed interval.
+
+```
+var tw = new TypeWriter('.example-1-output', {
+    text: 'I love printing text! Pity my intervals are the same each time.',
+    interval: 100
+});
+
+tw.type();
+```
+
+# Letter by letter, random natural human interval.
+
+```
+var tw = new TypeWriter('.example-2-output', {
+    text: 'I also love printing text. My intervals are randomised to look like a human is typing.',
+    interval: 'human'
+});
+
+tw.type();
+```
+
+# Word by work, fixed interval
+
+```
+var tw = new TypeWriter('.example-3-output', {
+    text: 'I LOVE PRINTING THE MOST!',
+    interval: 500,
+    words: true
+});
+
+tw.type();
+```
+
+# Letter by letter, random natural human interval with bounds specified
+# and a callback on completion.
+
+```
+var tw = new TypeWriter('.example-4-output', {
+    text: 'Woah! So glad this is the last example I have to sit through.',
+    interval: 'human',
+    lowerBound: 30,
+    upperBound: 130
+});
+
+tw.type(function () {
+    console.log('Finished typing now');
+});
+```
+
+# All options
+
+```
+var tw = new TypeWriter('selector', {
+    text: 'The text to write into the selector',
+    words: false, // Optional. Defaults to false.
+    interval: 'human' || 300, // Optional. Defaults to human.
+    lowerBound: 30 || null, // Optional. Defaults to 30ms
+    upperBound: 200 || null // Optional. Defaults to 200ms
+});
+
+tw.type(callback)
+```
--- TypeWriter.js
+++ TypeWriter.js
@@ -7,69 +7,91 @@
  *
  * Github:  http://github.com/ConnorAtherton/TypeWriter
  */
-(function(window, document) {
-  'use strict';
+ (function(window, document) {
+   'use strict';
 
-  var TypeWriter = TypeWriter || function (selector, opts) {
-    if(!selector) throw new Error('A selector must be specified');
-    if(!opts.text) throw new Error('TypeWriter needs text to type');
+   var TypeWriter = TypeWriter || function (selector, opts) {
+     if(!selector) throw new Error('A selector must be specified');
+     if(!opts.text) throw new Error('TypeWriter needs text to type');
 
-    var options = {
-      element: document.querySelector(selector),
-      text: opts.text,
-      words: opts.words || false,
-      interval: opts.interval || 'human',
-      lowerBound: 50,
-      upperBound: 170
-    },
-    randomIntFromInterval = function(min, max) {
-      return Math.floor(Math.random() * (max - min + 1) + min);
-    },
-    isNumber = function(n) {
-      return !isNaN(parseFloat(n)) && isFinite(n);
-    },
-    getIntervalSpeed = function () {
-      if(isNumber(options.interval))
-        return options.interval;
+     var options = {
+       element: document.querySelector(selector),
+       text: opts.text,
+       words: opts.words || false,
+       interval: opts.interval || 'human',
+       lowerBound: opts.lowerBound || 30,
+       upperBound: opts.upperBound || 200
+     },
+     randomIntFromInterval = function(min, max) {
+       return Math.floor(Math.random() * (max - min + 1) + min);
+     },
+     isNumber = function(n) {
+       return !isNaN(parseFloat(n)) && isFinite(n);
+     },
+     getIntervalSpeed = function () {
+       if(isNumber(options.interval))
+         return options.interval;
 
-      return randomIntFromInterval(options.lowerBound, options.upperBound);
-    },
-    typeByLetters = function (cb) {
-      var numberOfLetters = opts.text.length,
-          currentPosition = 0;
+       return randomIntFromInterval(options.lowerBound, options.upperBound);
+     },
+     typeByLettersConstantInterval = function (cb) {
+       var numberOfLetters = options.text.length,
+           currentPosition = 0;
 
-      var interval = window.setInterval(function () {
-        if(currentPosition === numberOfLetters){
-          window.clearInterval(interval);
-          cb && cb.call(window);
-        } else {
-          options.element.innerHTML += options.text[currentPosition];
-          currentPosition++;
-        }
-      }, getIntervalSpeed.call() )
-    },
-    typeByWords = function (cb) {
-      var words = options.text.split(' '),
-          numberOfWords = words.length,
-          currentPosition = 0;
+       var interval = window.setInterval(function () {
+         if(currentPosition === numberOfLetters){
+           window.clearInterval(interval);
+           cb && cb.call(window);
+         } else {
+           options.element.innerHTML += options.text[currentPosition];
+           currentPosition++;
+         }
+       }, getIntervalSpeed.call() )
+     },
+     typeByLettersRandomisedInterval = function (cb) {
+       var numberOfLetters = options.text.length,
+           currentPosition = 0;
 
-      var interval = window.setInterval(function () {
-        if(currentPosition === numberOfWords){
-          window.clearInterval(interval);
-          cb && cb.call(window);
-        } else {
-          options.element.innerHTML += (words[currentPosition] + ' ');
-          currentPosition++;
-        }
-      }, getIntervalSpeed.call() )
-    }
+       repeat(numberOfLetters, currentPosition, cb);
+     },
+     repeat = function (numberOfLetters, currentPosition, cb) {
+       if(numberOfLetters === 0)
+         return cb && cb.call(window);
 
-    this.type = function (cb) {
-      options.words ? typeByWords(cb) : typeByLetters(cb);
-    };
+       var interval = getIntervalSpeed.call(),
+           timer;
 
-  };
+       options.element.innerHTML += options.text[currentPosition];
 
-  this.TypeWriter = TypeWriter;
+       timer = setTimeout(function () {
+         numberOfLetters--; currentPosition++;
+         repeat(numberOfLetters, currentPosition, cb);
+       }, interval);
+     },
+     typeByWords = function (cb) {
+       var words = options.text.split(' '),
+           numberOfWords = words.length,
+           currentPosition = 0;
 
-}).call(this, window, document)
+       var interval = window.setInterval(function () {
+         if(currentPosition === numberOfWords){
+           window.clearInterval(interval);
+           cb && cb.call(window);
+         } else {
+           options.element.innerHTML += (words[currentPosition] + ' ');
+           currentPosition++;
+         }
+       }, getIntervalSpeed.call() )
+     }
+
+     this.type = function (cb) {
+       options.words ? typeByWords(cb) :
+           isNumber(options.interval) ? typeByLettersConstantInterval(cb) :
+                                        typeByLettersRandomisedInterval(cb);
+     };
+
+   };
+
+   this.TypeWriter = TypeWriter;
+
+ }).call(this, window, document)
